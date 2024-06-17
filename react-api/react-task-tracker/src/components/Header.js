e6ec3ad82a3e8ba2f08eef2660fcce44b76f391e
cmstover <cstover@partechgss.com>
2024-06-17 16:58:14 -0400

    adding css styling for task tracker

---
+++ react-api/react-task-tracker/src/components/Header.js
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types'
+
+
+const Header = ({ title }) => {
+  return (
+   <header className="header">
+    <h1>{title}</h1>
+    <button>Add</button>
+   </header>
+  )
+}
+
+Header.defaultProps = {
+  title:'Task Tracker',
+}
+
+Header.prototype = {
+ title: PropTypes.string.isRequired,
+}
+
+
+export default Header
