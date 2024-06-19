12c4642665ebc792f591e5c08b4bfc8ed7915f3e
cmstover <cstover@partechgss.com>
2024-06-19 16:27:08 -0400

    Deleting task

--- react-api/react-task-tracker/src/App.js
+++ react-api/react-task-tracker/src/App.js
@@ -1,9 +1,40 @@
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { useState } from 'react'
+
 
 function App() {
+
+ const [tasks , setTasks] = useState([
+  {
+    id: 1,
+    text:'laundry', 
+    day: 'June 19th at 2:00pm',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text:'fix washing machine',
+    day: 'June 21st at 5:30pm',
+    reminder: true,
+  }, 
+  {
+    id: 3,
+    text: 'nap',
+    day: 'June 22nd at 1:00pm',
+    reminder: false,
+  }]);
+
+  // delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+
   return (
     <div className="container">
       <Header  />
+      <Tasks tasks={tasks} onDelete={deleteTask}/>
     </div>
   );
 }
