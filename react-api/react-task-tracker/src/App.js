dc24a95b26030f1c2987b385267a5c2acab17051
Codiemaureen <codiestover.cs@gmail.com>
2024-06-20 13:54:07 -0400

    Adding reminder value to the database

--- react-api/react-task-tracker/src/App.js
+++ react-api/react-task-tracker/src/App.js
@@ -22,6 +22,14 @@ const App = () => {
     const data = await res.json()
     return data;
   }
+
+
+    const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await res.json()
+    return data;
+  }
+
   // add task
   const addTask = async (task) => {
     const res = await fetch(`http://localhost:5000/tasks/`, {
@@ -45,8 +53,23 @@ const App = () => {
   }
 
   // toggle reminder
-  const toggleReminder = (id) => {
-    setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id)
+    const updTask = {...taskToToggle,
+      reminder: !taskToToggle.reminder
+    }
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(updTask)}
+    )
+
+    const data = await res.json()
+
+    setTasks(tasks.map((task) => task.id === id ? {...task, reminder: data.reminder} : task))
   }
 
   return (
