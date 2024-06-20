b483042631b6c74e4b6e5753da93387f70a6b5e1
Codiemaureen <codiestover.cs@gmail.com>
2024-06-20 14:36:29 -0400

    Adding Routes

--- react-api/react-task-tracker/src/App.js
+++ react-api/react-task-tracker/src/App.js
@@ -1,8 +1,10 @@
+import { useEffect, useState } from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
-import { useEffect, useState } from 'react'
-
+import Footer from "./components/Footer";
+import About from "./components/About";
 
 const App = () => {
  const [showAddTask, setShowAddTask] = useState(false)
@@ -71,13 +73,32 @@ const App = () => {
 
     setTasks(tasks.map((task) => task.id === id ? {...task, reminder: data.reminder} : task))
   }
-
   return (
-    <div className="container">
-      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
-      {showAddTask && <AddTask onAdd={addTask}/>}
-      <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
-    </div>
+    <Router>
+      <div className="container">
+        <Header
+         onAdd={() => setShowAddTask(!showAddTask)} 
+         showAdd={showAddTask}/>
+        <Routes>
+          <Route
+          path="/"
+          element={
+            <>
+              {showAddTask && <AddTask onAdd={addTask}/>}
+              {tasks.length > 0 ? 
+              (<Tasks tasks={tasks} 
+                onDelete={deleteTask} 
+                onToggle={toggleReminder}/>) : (
+                'No task to show'
+              )}
+            </>            
+          }
+          />
+          <Route path='/about' element={<About />}/>
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ react-api/react-task-tracker/src/components/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <div>
+      <h4>Version 2.5.4</h4>
+      <a href="/">Home</a>
+    </div>
+  )
+}
+
+export default About
